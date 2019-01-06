27ed8412f68da0a0fc3b383c1744b609dd489af1
Nicolas Hennick <nicolashennick@gmail.com>
2019-01-06 17:28:25 +0000

    wrote first date test

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -21,4 +21,70 @@ RSpec.describe Project do
       expect(project).to be_done
     end
   end
+
+  describe "estimates" do
+    let(:project) { Project.new }
+    let(:done) { Task.new(size: 2, completed: true) }
+    let(:small_not_done) { Task.new(size: 1) }
+    let(:large_not_done) { Task.new(size: 4) }
+
+    before(:example) do
+      project.tasks = [done, small_not_done, large_not_done]
+    end
+
+    it "can calculate total size" do
+      expect(project.total_size).to eq(7)
+    end
+
+    it "can calculate remaining size" do
+      expect(project.remaining_size).to eq(5)
+    end
+  end
+
+  describe "velocity" do
+    let(:task) { Task.new(size: 3) }
+
+    it "does not count an incomplete task toward velocity" do
+      expect(task).not_to be_part_of_velocity
+      expect(task.points_toward_velocity).to eq(0)
+    end
+
+    it "does not count a long-ago task toward velocity" do
+      task.mark_completed(6.months.ago)
+      expect(task).not_to be_part_of_velocity
+      expect(task.points_toward_velocity).to eq(0)
+    end
+
+    it "counts a recently completed task toward velocity" do
+      task.mark_completed(1.day.ago)
+      expect(task).to be_part_of_velocity
+      expect(task.points_toward_velocity).to eq(3)
+    end
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
